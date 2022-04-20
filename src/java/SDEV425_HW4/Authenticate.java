0591dee4fa41b89f7f0d3e0b211a7590081b6b21
Michael Lee Galyen jr <36580986+MikeGalyen@users.noreply.github.com>
2022-04-20 14:36:25 -0400

    Update Authenticate.java

--- src/java/SDEV425_HW4/Authenticate.java
+++ src/java/SDEV425_HW4/Authenticate.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package SDEV425_HW4;
 
 import java.io.IOException;
@@ -20,10 +16,7 @@ import java.sql.PreparedStatement;
 import jakarta.servlet.Filter;
 import org.apache.derby.jdbc.ClientDataSource;
 
-/**
- *
- * @author jim
- */
+
 public class Authenticate extends HttpServlet {
 
     // variables    
