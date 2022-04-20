d20c9a3588d0f42b9ec9d4c90f0a0f0e45e1742b
MikeGalyen <mkgalyen@gmail.com>
2022-04-20 14:26:19 -0400

    updates comments in .java files

--- src/java/SDEV425_HW4/ShowAccount.java
+++ src/java/SDEV425_HW4/ShowAccount.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package SDEV425_HW4;
 
 import java.io.IOException;
@@ -29,8 +25,7 @@ import java.time.format.DateTimeFormatter;
 import org.apache.derby.jdbc.ClientDataSource;
 
 /**
- *
- * @author jim
+ * Class serves a page with user account data
  */
 public class ShowAccount extends HttpServlet {
 
